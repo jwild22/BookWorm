c6d369c668b2585a9c71c53d995d4ae2a02cef70
Farukh Kanzhayev <lasamasa24@gmail.com>
2024-10-10 15:35:07 +0500

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Farukh K on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
